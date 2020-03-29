b8eb1e3e1fa6d87345dfd86847d052e5896d0da2
tamlt2704 <tamlt2704@gmail.com>
2020-03-29 11:13:59 +0000

    add bus api and bus data

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const bus_util = require("../utils/bus_utils.js");
 
 var app = express.Router();
 
 app.get('/time', (req, res) => {
     res.json({'time': new Date()});
+});
+
+app.get('/bus', (req, res) => {
+   res.json(bus_util.getBusNumbers()); 
+});
+
+app.get('/bus/:busno', (req, res) => {
+    var busno = req.params.busno;
+    var busdata = bus_util.readBusData(busno);
+    res.json(busdata);
 });
 
 module.exports = app;
