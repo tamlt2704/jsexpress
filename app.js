6600d660871647491a937ec769e4e0db37bd5cdd
tamlt2704 <tamlt2704@gmail.com>
2020-03-29 07:27:28 +0000

    use reactjs-index.html

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const logger = require("morgan");
+const path = require("path");
 
 
 var app = express();
 
 app.use(logger("dev"));
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res) => { res.send("Hello world"); });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
